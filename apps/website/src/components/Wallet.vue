1d7bb3049170f1280eebfdb743c4ec8b893deeed
Christopher Cali <chris@consensusnetworks.com>
2022-07-13 14:35:23 -0400

    Show transaction from metamask wallet to coinbase wallet (ignore css)

--- apps/website/src/components/Wallet.vue
+++ apps/website/src/components/Wallet.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
-    <div class="metamask-div">
-      <button ref="connectMetamaskButton" @click="connectWallet('metamask')">
-        connect-metamask
-      </button>
-      <p>
-        Connected Metamsk Account:
-        <span ref="metamaskAccountsResult" />
-      </p>
+    <div class="connect-wallet-container">
+      <div class="metamask-div">
+        <button ref="connectMetamaskButton" @click="connectWallet('metamask')">
+          connect-metamask
+        </button>
+        <p>
+          Connected Metamsk Account:
+          <span ref="metamaskAccountsResult">Address Not Active</span>
+        </p>
+      </div>
+      <div class="coinbase-div">
+        <button
+          ref="connectCoinbaseButton"
+          class="coinbase-btn"
+          @click="connectWallet('coinbase')"
+        >
+          Connect Coinbase
+        </button>
+        <p>
+          Connected Coinbase Account:
+          <span ref="coinbaseAccountsResult">Address Not Active</span>
+        </p>
+      </div>
     </div>
-    <div class="coinbase-div">
-      <button
-        ref="connectCoinbaseButton"
-        class="coinbase-btn"
-        @click="connectWallet('coinbase')"
-      >
-        connect-coinbase
-      </button>
-      <p>
-        Connected Coinbase Account:
-        <span ref="coinbaseAccountsResult" />
-      </p>
+    <div class="form-container">
+      <form @submit.prevent="sendTransaction">
+        <label for="address">Address</label>
+        <input v-model="mmToAddress" type="text" placeholder="To Address" />
+        <br />
+        <label for="amount">Amount</label>
+        <input v-model="mmAmount" type="text" placeholder="Amount Ether" />
+        <button type="submit">
+          Send Transaction
+        </button>
+      </form>
     </div>
   </div>
 </template>
@@ -34,6 +48,8 @@ const connectMetamaskButton: Ref = ref<HTMLDivElement>()
 const connectCoinbaseButton: Ref = ref<HTMLDivElement>()
 const metamaskAccountsResult: Ref = ref<HTMLSpanElement>()
 const coinbaseAccountsResult: Ref = ref<HTMLSpanElement>()
+const mmToAddress = ref<string>('') // Test to address: 0xD4e5faa8aD7d499Aa03BDDE2a3116E66bc8F8203
+const mmAmount = ref<string>('')
 
 const selectedProvider: any = ref({})
 const ethereum: any = window.ethereum
@@ -54,22 +70,41 @@ const connectWallet = async (provider: string) => {
     })
 
     if (provider === 'metamask') {
-      let p = document.createElement('p')
-      p.innerText = account[0]
       metamaskAccountsResult.value.innerText = ''
-      metamaskAccountsResult.value.appendChild(p)
+      metamaskAccountsResult.value.innerText = account[0]
+      coinbaseAccountsResult.value.innerText = 'Not Active'
+      connectCoinbaseButton.value.innerHTML = 'Activate Coinbase'
+      connectMetamaskButton.value.innerHTML = 'Metamask Connected'
     } else if (provider === 'coinbase') {
-      let p = document.createElement('p')
-      p.innerText = account[0]
       coinbaseAccountsResult.value.innerText = ''
-      coinbaseAccountsResult.value.appendChild(p)
+      coinbaseAccountsResult.value.innerText = account[0]
+      connectCoinbaseButton.value.innerHTML = 'Coinbase Connected'
+      connectMetamaskButton.value.innerHTML = 'Activate Metamask'
+      metamaskAccountsResult.value.innerText = 'Not Active'
     }
 
     const web3Provider: ethers.providers.Web3Provider = new ethers.providers.Web3Provider(
       selectedProvider.value
     )
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-    connectMetamaskButton.value.innerHTML = 'Metamask connected'
+const sendTransaction = async (event: any) => {
+  try {
+    const web3Provider: ethers.providers.Web3Provider = new ethers.providers.Web3Provider(
+      selectedProvider.value
+    )
+    const signer = web3Provider.getSigner()
+    const etherAmount = ethers.utils.parseEther(mmAmount.value)
+    const tx = {
+      to: mmToAddress.value,
+      value: etherAmount,
+    }
+    signer.sendTransaction(tx).then((txObj) => {
+      console.log('successful txHash: ', txObj.hash)
+    })
   } catch (error) {
     console.error(error)
   }
@@ -119,9 +154,48 @@ button {
   width: 100%;
   margin-top: 1rem;
   margin-bottom: 1rem;
+  width: 750px;
 }
 
 .coinbase-btn {
   background-color: blue;
+}
+
+.connect-wallet-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5rem;
+  border: 1px solid lightblue;
+  padding: 2rem;
+  width: 50%;
+  /* center in middle of screen */
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5rem;
+  border: 1px solid lightblue;
+  padding: 2rem;
+  width: 50%;
+  /* center in middle of screen */
+  margin-left: auto;
+  margin-right: auto;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  width: 500px;
+  padding: 1rem;
 }
 </style>
