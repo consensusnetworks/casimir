1190576b0079c272f7b6d8f1a2c0e86b361a2f51
Christopher Cali <chris@consensusnetworks.com>
2022-07-13 11:52:23 -0400

    Show ability to connect separate accounts from separate wallet providers

--- apps/website/src/components/Wallet.vue
+++ apps/website/src/components/Wallet.vue
@@ -1,43 +1,27 @@
 <template>
   <div>
     <div class="metamask-div">
-      <button ref="connectMetamaskButton" @click="onClickConnect('metamask')">
+      <button ref="connectMetamaskButton" @click="connectWallet('metamask')">
         connect-metamask
       </button>
-      <button ref="getAccountsButton" @click="showAccounts">
-        eth_accounts
-      </button>
       <p>
-        connected eth_accounts: <span ref="getAccountsResult">None yet</span>
+        Connected Metamsk Account:
+        <span ref="metamaskAccountsResult" />
       </p>
-      <button ref="disconnectMetamaskButton" @click="disconnectMetamask">
-        disconnect-metamask
-      </button>
     </div>
-    <!-- Coinbase wallet -->
     <div class="coinbase-div">
       <button
         ref="connectCoinbaseButton"
         class="coinbase-btn"
-        @click="onClickConnect('coinbase')"
+        @click="connectWallet('coinbase')"
       >
         connect-coinbase
       </button>
-      <button ref="getCoinbaseButton" class="coinbase-btn">
-        coinbase_accounts
-      </button>
       <p>
-        connected coinbase_accounts:
-        <span ref="getCoinbaseResult">None yet</span>
+        Connected Coinbase Account:
+        <span ref="coinbaseAccountsResult" />
       </p>
     </div>
-    <button
-      ref="disconnectCoinbaseButton"
-      class="coinbase-btn"
-      @click="disconnectCoinbase"
-    >
-      disconnect-coinbase
-    </button>
   </div>
 </template>
 
@@ -45,13 +29,15 @@
 import { onMounted, Ref, ref } from 'vue'
 import { WalletProvider } from '@/interfaces/WalletProvider'
 import { Web3Provider } from '@/interfaces/Web3Provider'
-/** Start Multiple Provider Configuration **/
 import { ethers } from 'ethers'
 
-const selectedProvider: any = ref({})
+const connectMetamaskButton: Ref = ref<HTMLDivElement>()
+const connectCoinbaseButton: Ref = ref<HTMLDivElement>()
+const metamaskAccountsResult: Ref = ref<HTMLSpanElement>()
+const coinbaseAccountsResult: Ref = ref<HTMLSpanElement>()
 
+const selectedProvider: any = ref({})
 const ethereum: any = window.ethereum
-// 'metamask', coinbase
 const availableProviders: { [key: string]: WalletProvider } = {
   metamask: ethereum.providers.find(
     (provider: WalletProvider) => provider.isMetaMask
@@ -60,44 +46,31 @@ const availableProviders: { [key: string]: WalletProvider } = {
     (provider: WalletProvider) => provider.isCoinbaseWallet
   ),
 }
-/** End Multiple Provider Configuration **/
 
-const forwarderOrigin = 'http://localhost:9010'
-const connectMetamaskButton: Ref = ref<HTMLDivElement>()
-const getAccountsButton: Ref = ref<HTMLButtonElement>()
-const getAccountsResult: Ref = ref<HTMLSpanElement>()
-const disconnectMetamaskButton: Ref = ref<HTMLButtonElement>()
-
-const disconnectMetamask = async () => {
-  // Disconnect metamask
-}
-
-const disconnectCoinbase = async () => {
-  // Disconnect coinbase wallet
-  console.log('clicked!')
-  // coinbaseWallet.disconnect()
-}
-
-const onClickConnect = async (provider: string) => {
+const connectWallet = async (provider: string) => {
   try {
     selectedProvider.value = availableProviders[provider]
     const account = await selectedProvider.value.request({
       method: 'eth_requestAccounts',
     })
-    console.log('account in onClickConnect :>> ', account)
+
+    if (provider === 'metamask') {
+      let p = document.createElement('p')
+      p.innerText = account[0]
+      metamaskAccountsResult.value.innerText = ''
+      metamaskAccountsResult.value.appendChild(p)
+    } else if (provider === 'coinbase') {
+      let p = document.createElement('p')
+      p.innerText = account[0]
+      coinbaseAccountsResult.value.innerText = ''
+      coinbaseAccountsResult.value.appendChild(p)
+    }
 
     const web3Provider: ethers.providers.Web3Provider = new ethers.providers.Web3Provider(
       selectedProvider.value
     )
 
-    console.log(web3Provider)
-    // const accounts = await ethereum.request({ method: 'eth_accounts' })
-    // console.log('accounts in onClickConnect :>> ', accounts)
-    // TODO: Disable this button while the request is pending!
-    // eslint-disable-next-line no-undef
     connectMetamaskButton.value.innerHTML = 'Metamask connected'
-    // console.log('ethereum.networkVersion :>> ', ethereum.networkVersion)
-    // console.log('ethereum.selectedAddress :>> ', ethereum.selectedAddress)
   } catch (error) {
     console.error(error)
   }
@@ -105,80 +78,14 @@ const onClickConnect = async (provider: string) => {
 
 onMounted(() => {
   console.log('availableProviders :>> ', availableProviders)
-  // const onboarding = new MetaMaskOnboarding({ forwarderOrigin })
 
-  // const onClickConnect = async (provider) => {
-  //   try {
-  //     selectedProvider.value = availableProviders[provider]
-  //     const account = await selectedProvider.value.request({
-  //       method: 'eth_requestAccounts',
-  //     })
-  //     console.log('account in onClickConnect :>> ', account)
-  //     const web3Provider = new ethers.providers.Web3Provider(provider)
-  //     // const accounts = await ethereum.request({ method: 'eth_accounts' })
-  //     // console.log('accounts in onClickConnect :>> ', accounts)
-  //     // TODO: Disable this button while the request is pending!
-  //     // eslint-disable-next-line no-undef
-  //     const isMetaMask = await detectEthereumProvider({ mustBeMetaMask: true })
-  //     if (!isMetaMask) {
-  //       console.log('MetaMask not detected')
-  //       throw new Error('Metamask not detected')
-  //     }
-  //     connectMetamaskButton.value.innerHTML = 'Metamask connected'
-  //     // console.log('ethereum.networkVersion :>> ', ethereum.networkVersion)
-  //     // console.log('ethereum.selectedAddress :>> ', ethereum.selectedAddress)
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
-
-  const onClickInstall = () => {
-    console.log('onClickInstall')
-    connectMetamaskButton.value.innerText =
-      'Refresh page after installing Metamask'
-    connectMetamaskButton.value.disabled = true
-    // Starts the onboarding process for our end user
-    onboarding.startOnboarding()
-  }
-
+  // Optional TODO: Set a method that installs metamask for user (if not already installed).
   if (!isMetaMaskInstalled()) {
     console.log('MetaMask not installed')
-    connectMetamaskButton.value.innerText = 'Click here to install MetaMask!'
-    connectMetamaskButton.value.onclick = onClickInstall
-    connectMetamaskButton.value.disabled = false
   } else {
     console.log('MetaMask is installed')
-    connectMetamaskButton.value.innerText = 'Connect Metamask'
-    // connectMetamaskButton.value.onclick = onClickConnect
-    connectMetamaskButton.value.disabled = false
   }
-
-  disconnectMetamaskButton.value.addEventListener('click', async () => {
-    const result = await disconnectMetamask()
-    console.log('result :>> ', result)
-  })
 })
-
-const showAccounts = async () => {
-  // eslint-disable-next-line no-undef
-  const accounts = await ethereum.request({ method: 'eth_accounts' })
-  // TODO: Determine how to get other accounts not currently selected in metamask UI
-  if (accounts.length > 0) {
-    accounts.forEach((account: string) => {
-      getAccountsResult.value.innerText = ''
-      let p = document.createElement('p')
-      p.innerText = account
-      getAccountsResult.value.appendChild(p)
-
-      // getCoinbaseResult.value.innerText = ''
-      // p = document.createElement('p')
-      // p.innerText = account
-      // getCoinbaseResult.value.appendChild(p)
-    })
-  } else {
-    getAccountsResult.value.innerText = 'No accounts found'
-  }
-}
 
 const isMetaMaskInstalled = () => {
   return Boolean(ethereum && ethereum.isMetaMask)
