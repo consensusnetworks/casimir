69a38663623ba5ca90b9f4e0398269e417a92df0
Shane Earley <shanejearley@gmail.com>
2022-08-04 10:57:17 -0400

    Fix cdk tsconfig to pass tests with json imports

--- infrastructure/cdk/test/website.test.ts
+++ infrastructure/cdk/test/website.test.ts
@@ -1,20 +1,30 @@
 import * as cdk from 'aws-cdk-lib'
 import { Template } from 'aws-cdk-lib/assertions'
 import { DnsStack } from '../lib/dns/dns-stack'
+import { EtlStack } from '../lib/etl/etl-stack'
 import { UsersStack } from '../lib/users/users-stack'
 import { WebsiteStack } from '../lib/website/website-stack'
 
 // Todo actually test something
 test('Website Created', () => {
 
+  const defaultEnv = { account: '257202027633', region: 'us-east-2' }
   const project = 'Casimir'
   const stage = 'Test'
 
   const app = new cdk.App()
-  const dnsStack = new DnsStack(app, `${project}DnsStack${stage}`, { project, stage })
+  const dnsStack = new DnsStack(app, `${project}DnsStack${stage}`, { env: defaultEnv, project, stage })
   const { domain, dnsRecords, hostedZone } = dnsStack
-  const websiteStack = new WebsiteStack(app, `${project}WebsiteStack${stage}`, { project, stage, domain, dnsRecords, hostedZone })
-  const usersStack = new UsersStack(app, `${project}UsersStack${stage}`, { project, stage, domain, dnsRecords, hostedZone })
+  const etlStack = new EtlStack(app, `${project}EtlStack${stage}`, { env: defaultEnv, project, stage })
+  const websiteStack = new WebsiteStack(app, `${project}WebsiteStack${stage}`, { env: defaultEnv, project, stage, domain, dnsRecords, hostedZone })
+  const usersStack = new UsersStack(app, `${project}UsersStack${stage}`, { env: defaultEnv, project, stage, domain, dnsRecords, hostedZone })
+
+  const etlTemplate = Template.fromStack(etlStack)
+  console.log(etlTemplate)
+  Object.keys(etlTemplate.findOutputs('*')).forEach(output => {
+    console.log(output)
+    expect(output).toBeDefined()
+  })
 
   const websiteTemplate = Template.fromStack(websiteStack)
   console.log(websiteTemplate)
