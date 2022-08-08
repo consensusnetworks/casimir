fedefd4776985d38aa1c76112a4e2c8a5443c4f6
Christopher Cali <chris@consensusnetworks.com>
2022-08-08 15:52:56 -0500

    Resolve vite + polyfill issue for @solana/web3.js package

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from 'vite'
 import { fileURLToPath } from 'url'
 import * as path from 'path'
 import pages from 'vite-plugin-pages'
-
+import inject from '@rollup/plugin-inject'
 import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill'
 import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
 
@@ -15,43 +15,38 @@ const config: UserConfig = {
       extensions: ['vue', 'md'],
     }),
   ],
-  define: {
-    'window.global': [],
-  },
   optimizeDeps: {
-    include: ['iotex-antenna'],
     esbuildOptions: {
       plugins: [
         NodeModulesPolyfills(),
         NodeGlobalsPolyfillPlugin({
           process: true,
-          buffer: true,
           define: true,
         }),
       ],
     },
+    include: ['iotex-antenna'],
   },
   build: {
     commonjsOptions: {
-      include: [/iotex-antenna/, /node_modules/]
-    }
+      include: [/iotex-antenna/, /node_modules/],
+    },
+    rollupOptions: {
+      plugins: [
+        inject({
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ],
+    },
   },
   resolve: {
     alias: {
       '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
       './runtimeConfig': './runtimeConfig.browser',
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
-  envPrefix: 'PUBLIC_'
+  envPrefix: 'PUBLIC_',
 }
 
 export default config
