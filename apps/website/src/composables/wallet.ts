0a40908795e0965dcb2a07674e9d62f51cb56e84
Christopher Cali <chris@consensusnetworks.com>
2022-08-11 11:23:50 -0500

    Phantom wallet successfully signingAndSending transactions

--- apps/website/src/composables/wallet.ts
+++ apps/website/src/composables/wallet.ts
@@ -5,9 +5,14 @@ import useEthers from '@/composables/ethers'
 import { BrowserProviders } from '@/interfaces/BrowserProviders'
 import { EthersProvider } from '@/interfaces/EthersProvider'
 import { ProviderString } from '@/types/ProviderString'
-import { Connection, Transaction, SystemProgram, PublicKey } from '@solana/web3.js'
+import {
+  Connection,
+  Transaction,
+  SystemProgram,
+  PublicKey,
+} from '@solana/web3.js'
 
-const amount = ref<string>('0.00001')
+const amount = ref<string>('1')
 const toAddress = ref<string>('7aVow9eVQjwn7Y4y7tAbPM1pfrE1TzjmJhxcRt8QwX5F')
 const { requestEthersAccount } = useEthers()
 
@@ -87,34 +92,23 @@ export default function useWallet() {
       } else if (solanaProvidersList.includes(provider)) {
         const network = 'https://api.devnet.solana.com'
         const connection = new Connection(network)
-
-        const enc = new TextEncoder() // always utf-8
-        const uint8ArrayFromAddress = enc.encode(selectedAccount.value)
-        console.log('selectedAccount.value :>> ', selectedAccount.value)
-        console.log('uint8ArrayFromAddress :>> ', uint8ArrayFromAddress)
+        const { blockhash } = await connection.getLatestBlockhash('finalized')
         const to = new PublicKey(toAddress.value)
         const from = new PublicKey(selectedAccount.value)
-
-        // const account = Base58.encode(new Buffer(selectedAccount.value))
+        const lamports = Number(amount.value) * 1000000000
         const transaction = new Transaction().add(
           SystemProgram.transfer({
-            // fromPubkey: Base58.encode(new Buffer(selectedAccount.value)),
             fromPubkey: from,
             toPubkey: to,
-            lamports: 10,
+            lamports,
           })
         )
-        // transaction.feePayer = solanaPublicKey.value
-        // const transaction = await createTransferTransaction(
-        //   selectedAccount.value,
-        //   connection
-        // )
-        console.log('transaction :>> ', transaction)
-        // const { signature } = await availableProviders.value[
-        //   'Phantom'
-        // ].signAndSendTransaction(transaction)
-        // console.log('signature :>> ', signature)
-        // await connection.getSignatureStatus(signature)
+        transaction.feePayer = from
+        transaction.recentBlockhash = blockhash
+        const { signature } = await availableProviders.value[
+          'Phantom'
+        ].signAndSendTransaction(transaction)
+        await connection.getSignatureStatus(signature)
       } else if (selectedProvider.value === 'IoPay') {
         await sendIoPayTransaction(toAddress.value, amount.value)
       } else {
