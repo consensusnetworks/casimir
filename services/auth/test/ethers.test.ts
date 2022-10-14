708c1e2bd03072d99ba69145693ea0a017de6f42
Shane Earley <shanejearley@gmail.com>
2022-10-13 21:50:43 -0400

    Cleanup verify message tests

--- services/auth/test/ethers.test.ts
+++ services/auth/test/ethers.test.ts
@@ -4,14 +4,14 @@ import useEthers from '../src/providers/ethers'
 
 const { verifyMessage } = useEthers()
 
-describe('Ethers provider', async function () {
+describe('Verify message', async function () {
 
-    it('Verify message fails if missing params', async function () {
+    it('Login credentials fail if missing params', async function () {
         const response = verifyMessage({ address: '', message: '', signedMessage: '' })
         expect(response).equal(false)
     })
 
-    it('Verify message succeeds if user signs the message', async function () {
+    it('Login credentials succeed if user signs the message', async function () {
         const [ signer ] = await ethers.getSigners()
         // @ccali sign an arbitrary message like in wallet code and replace message values below to pass
         const response = verifyMessage({ address: signer.address, message: '', signedMessage: '' })
