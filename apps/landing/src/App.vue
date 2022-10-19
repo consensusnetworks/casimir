54d98e64c4937a5907a84b422ae577b55b7e961b
demogorgod <ssnajeeb2020@manchester.edu>
2022-10-19 16:21:30 -0400

    Add Casimir typography and colors into app

--- apps/landing/src/App.vue
+++ apps/landing/src/App.vue
@@ -1,6 +1,10 @@
+<script lang="ts" setup>
+import NavBar from './components/nav/NavBar.vue'
+</script>
 <template>
-  <div>
+  <div class="bg-black text-white h-screen">
     <!-- No Layout needed -->
+    <NavBar />
     <RouterView />
   </div>
 </template>
