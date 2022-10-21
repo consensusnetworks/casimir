2495ca4dc301e42f426dc21f06cac4c51869423e
Shane Earley <shanejearley@gmail.com>
2022-10-21 13:50:31 -0400

    Switch router import to ts

--- apps/landing/src/main.ts
+++ apps/landing/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import '@/index.css'
-import router from './composables/router.js'
+import router from './composables/router'
 
 const app = createApp(App)
 app.use(router)
