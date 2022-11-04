304eb163ec1c1737191a5f95949cd6dff6aebb30
Shane Earley <shanejearley@gmail.com>
2022-11-04 09:28:50 -0400

    Change mining interval

--- contracts/ethereum/hardhat.config.ts
+++ contracts/ethereum/hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
       forking: forkUrl ? { url: forkUrl } : undefined,
       mining: {
         auto: false,
-        interval: 1000
+        interval: 12000
       }
     }
   }
