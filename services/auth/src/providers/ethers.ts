02b8b33bc78d3054bb55348f31b33a36658dc9f1
Christopher Cali <chris@consensusnetworks.com>
2022-12-01 15:56:47 -0600

    Remove unnecessary code in services auth thers.ts

--- services/auth/src/providers/ethers.ts
+++ services/auth/src/providers/ethers.ts
@@ -1,7 +1,5 @@
 import { ethers } from 'ethers'
 import { LoginCredentials } from '@casimir/types'
-import useUsers from './users'
-const { getMessage } = useUsers()
 
 export default function useEthers() {
 
@@ -13,16 +11,12 @@ export default function useEthers() {
      */
     function verifyMessage(loginCredentials: LoginCredentials): boolean {
         const { address, message, signedMessage } = loginCredentials
-        const existingMessage = getMessage(address)
-        if (existingMessage !== message) {
-            console.log(`Message mismatch: ${existingMessage} !== ${message}`)
-            return false // TODO: do we want to throw a specific error here to handle better? Would have to change type.
-        }
         try {
             if (!address.length || !message.length || !signedMessage.length) {
                 return false
             } else {
                 const recoveredAddress = ethers.utils.verifyMessage(message, signedMessage)
+                console.log('recoveredAddress :>> ', recoveredAddress)
                 return address.toLowerCase() === recoveredAddress.toLowerCase()
             }
         } catch (error) {
