4a0fa55dd46d3b4f63ca60a2b9bfa83c479501c1
Shane Earley <shanejearley@gmail.com>
2023-01-10 16:43:05 -0500

    Fix import in keygen validator test

--- services/keygen/test/validator.test.ts
+++ services/keygen/test/validator.test.ts
@@ -7,8 +7,8 @@ const withdrawalAddress = '0x4eAfaAC934dC9CB2288e188f61A27C28dABF693B'
 describe('Validator', async function () {    
 
     it('Provides 4 operators', async function () {
-        const { getValidator } = useValidator()
-        const { operators } = await getValidator(withdrawalAddress)
+        const { getValidatorInit } = useValidator()
+        const { operators } = await getValidatorInit(withdrawalAddress)
         expect(operators.length).equal(4)
     })
 
