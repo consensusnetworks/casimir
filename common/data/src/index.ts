be88a17b5bd1f21ffd14d9b9c66dbf531b44b285
hawyar <hawyarfa@gmail.com>
2023-03-04 23:32:32 -0500

    Update event table schema

--- common/data/src/index.ts
+++ common/data/src/index.ts
@@ -22,18 +22,9 @@ export function schemaToGlueColumns(jsonSchema: JsonSchema): glue.Column[] {
     let type: glue.Type = glue.Schema[typeKey]
 
     if (name.endsWith('_at')) type = glue.Schema.TIMESTAMP
-
-    if (name.endsWith('_list')) type = glue.Schema.array(glue.Schema.STRING)
-
-    if (name.endsWith('amount')) type = glue.Schema.BIG_INT
-
-    if (name === 'gas_used') type = glue.Schema.BIG_INT
-
-    if (name === 'gas_limit') type = glue.Schema.BIG_INT
-
-    if (name === 'base_fee') type = glue.Schema.BIG_INT
-
-    if (name === 'burnt_fee') type = glue.Schema.FLOAT
+    if (name.endsWith('_balance')) type = glue.Schema.BIG_INT
+    if (name == 'amount') type = glue.Schema.BIG_INT
+    if (name === 'price') type = glue.Schema.FLOAT
 
     const comment = property.description
     return { name, type, comment }
@@ -41,44 +32,34 @@ export function schemaToGlueColumns(jsonSchema: JsonSchema): glue.Column[] {
 }
 
 export type EventTableSchema = {
-  /** Name of the chain (e.g. iotex, ethereum) */
-  chain: string
-  /** Name of the network (e.g. mainnet, testnet) */
-  network: string
-  /** "Name of the provider (e.g. casimir, infura, alchemy) */
-  provider: string
-  /** The type of event (e.g. block, transaction, deposit) */
-  type: string
-  /** The block height */
-  height: number
-  /** The block hash */
-  block: string
-  /** The transaction hash */
-  transaction: string
-  /** The date timestamp of the event in ISO 8601 format (e.g. 2015-03-04T22:44:30.652Z) */
-  created_at: string
-  /** The address which initiated the event, a miner in case of block and a caller in case of other events */
-  address: string
-  /** The recipient's address */
-  to_address: string
-  /** The amount value associated with the transaction */
-  amount: string
-  /** The total amount of gas used  */
-  gasUsed: string
-  /** The gas limit provided by transactions in the block */
-  gasLimit: string
-  /** Post-London upgrade this represents the minimum gasUsed multiplier required for a transaction to be included in a block */
-  baseFee: string
-  /** Post-London Upgrade, this represents the part of the tx fee that is burnt */
-  burntFee: string
-  /** The validator's address */
-  validator: string
-  /** The list of validators' addresses */
-  validator_list: string[]
-  /** The duration of the event */
-  duration: number
-  /** Is auto staking enabled */
-  auto_stake: boolean
+  // The chain which the event belongs to (e.g. iotex, ethereum)
+  chain:'etheruem' | 'iotex';
+  // The network which the event was received on (e.g. mainnet, testnet)
+  network: 'mainnet' | 'testnet' | 'goerli';
+  // The provider used to source the event (e.g. infura, consensus)
+  provider: 'alchemy' | 'consensus';
+  // The type of event (e.g. block, transaction)
+  type: 'block' | 'transaction';
+  // The height of the block the event belongs to
+  height: number;
+  // The block hash
+  block: string;
+  // The transaction hash
+  transaction: string;
+  // The timestamp of the event recieved by the blockchain (format: Modified ISO 8601 e.g. 2015-03-04 22:44:30.652)"
+  receivedAt: string;
+  // The sender's address
+  sender: string;
+  // The recipient's address
+  recipient: string;
+  // The sender's balance at the time of the event
+  senderBalance: string;
+  // The recipient's balance at the time of the event
+  recipientBalance: string;
+  // The amount transferred in the event
+  amount: string;
+  // The exchange price of the coin at the time of the event
+  price: number;
 }
 
-export { eventSchema, aggSchema, operatorStore, validatorStore }
+export { eventSchema, aggSchema, operatorStore, validatorStore}
