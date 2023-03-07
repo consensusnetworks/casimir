761ad99b6578c1de1400412284821921bd16f8f3
demogorgod <ssnajeeb2020@manchester.edu>
2023-03-06 22:33:06 -0500

    Connect to new auth, and adjust new layout

--- apps/web/src/layouts/default-layout.vue
+++ apps/web/src/layouts/default-layout.vue
@@ -23,7 +23,7 @@ import TopNav from '@/components/navigation/TopNav.vue'
 
 <template>
   <div class="h-[100vh] overflow-hidden">
-    <div class="h-full flex flex-col sr-only noscreen:not-sr-only ">
+    <div class="h-full flex flex-col">
       <div class="min-w-[360px] w-full">
         <TopNav />
       </div>
@@ -36,19 +36,19 @@ import TopNav from '@/components/navigation/TopNav.vue'
           </div>
         </div>
       </div>
-    </div>
+      
+      <div class="visible absolute noscreen:invisible top-0 left-0 h-full w-full bg-white flex flex-col justify-center items-center text-center px-20">
+        <i
+          class="iconoir-enlarge-round-arrow text-[36px] text-grey_4 mb-10"
+        />
+        <h6 class="text-grey_4 font-bold mb-20">
+          Casimir does not support this size of screen
+        </h6>
 
-    <div class="not-sr-only noscreen:sr-only h-full w-full flex flex-col justify-center items-center text-center px-20">
-      <i
-        class="iconoir-enlarge-round-arrow text-[36px] text-grey_4 mb-10"
-      />
-      <h6 class="text-grey_4 font-bold mb-20">
-        Casimir does not support this size of screen
-      </h6>
-
-      <h6 class="text-grey_3">
-        Please rotate to a landscape version or use a different device
-      </h6>
+        <h6 class="text-grey_3">
+          Please rotate to a landscape version or use a different device
+        </h6>
+      </div>
     </div>
   </div>
 </template>
