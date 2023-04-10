76e09c9f4771b584074c43f07c8c109d697edaf2
Christopher Cali <chris@consensusnetworks.com>
2023-04-10 18:54:23 -0400

    Add auth check in router

--- apps/web/src/composables/router.ts
+++ apps/web/src/composables/router.ts
@@ -1,4 +1,5 @@
 import { createWebHistory, createRouter } from 'vue-router'
+import useUsers from './users'
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import Dashboard from '@/pages/dashboard/Dashboard.vue'
@@ -31,5 +32,12 @@ const router = createRouter({
 
 // TO DO: Add a routing beforeEach that 
 // dynamically fixes rerouting to auth page
+
+router.beforeEach((to, from, next) => {
+    const { checkUserSessionExists } = useUsers()
+    const loggedIn = checkUserSessionExists()
+    if (to.name !== 'Auth' && !loggedIn) next({ name: 'Auth' })
+    else next()
+})
 
 export default router
