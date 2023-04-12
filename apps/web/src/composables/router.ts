ee14ec0c5689e55632451e2851e80d4beb09d534
demogorgod <ssnajeeb2020@manchester.edu>
2023-04-12 14:50:55 -0400

    Add `/:pathMatch(.*)*` to the possible routes to reroute to dashboard

--- apps/web/src/composables/router.ts
+++ apps/web/src/composables/router.ts
@@ -19,6 +19,11 @@ const routes = [
         component: Auth,
     },
     { 
+        path: '/:pathMatch(.*)*', 
+        name: Dashboard, 
+        component: Dashboard,
+    },
+    { 
         path: '/test', 
         name: Test, 
         component: Test,
