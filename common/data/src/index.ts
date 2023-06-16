6ebd744afb8a57fd53b0da954823ad8e9967da8a
hawyar <hawyarfa@gmail.com>
2023-06-16 13:34:27 -0400

    Removed agg resource, added resource for wallet and staking_action tables

--- common/data/src/index.ts
+++ common/data/src/index.ts
@@ -2,7 +2,8 @@ import accountSchema from './schemas/account.schema.json'
 import nonceSchema from './schemas/nonce.schema.json'
 import userSchema from './schemas/user.schema.json'
 import eventSchema from './schemas/event.schema.json'
-import aggSchema from './schemas/agg.schema.json'
+import walletSchema from './schemas/wallet.schema.json'
+import stakingActionSchema from './schemas/staking_action.schema.json'
 import userAccountSchema from './schemas/user_account.schema.json'
 import operatorStore from './mock/operator.store.json'
 import validatorStore from './mock/validator.store.json'
@@ -18,7 +19,8 @@ export {
   userSchema,
   userAccountSchema,
   eventSchema,
-  aggSchema,
+  walletSchema,
+  stakingActionSchema,
   accountStore,
   userStore,
   operatorStore,
--- infrastructure/cdk/src/providers/etl.ts
+++ infrastructure/cdk/src/providers/etl.ts
@@ -2,7 +2,7 @@ import { Construct } from 'constructs'
 import * as cdk from 'aws-cdk-lib'
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as glue from '@aws-cdk/aws-glue-alpha'
-import { Schema, eventSchema, aggSchema } from '@casimir/data'
+import { Schema, eventSchema, walletSchema, stakingActionSchema } from '@casimir/data'
 import { kebabCase, pascalCase, snakeCase } from '@casimir/helpers'
 import { Config } from '../providers/config'
 import { EtlStackProps } from '../interfaces/StackProps'
@@ -21,7 +21,8 @@ export class EtlStack extends cdk.Stack {
 
         /** Get Glue Columns from JSON Schema for each table */
         const eventColumns = new Schema(eventSchema).getGlueColumns()
-        const aggColumns = new Schema(aggSchema).getGlueColumns()
+        const walletColumns = new Schema(walletSchema).getGlueColumns()
+        const stakingActionColumns = new Schema(stakingActionSchema).getGlueColumns()
 
         /** Create Glue DB */
         const database = new glue.Database(this, config.getFullStackResourceName(this.name, 'database'), {
@@ -30,7 +31,10 @@ export class EtlStack extends cdk.Stack {
 
         /** Create S3 buckets */
         const eventBucket = new s3.Bucket(this, config.getFullStackResourceName(this.name, 'event-bucket'))
-        const aggBucket = new s3.Bucket(this, config.getFullStackResourceName(this.name, 'agg-bucket'))
+        const walletBucket = new s3.Bucket(this, config.getFullStackResourceName(this.name, 'wallet-bucket'))
+        const stakingActionBucket = new s3.Bucket(this, config.getFullStackResourceName(this.name, 'staking-action-bucket'))
+
+
         new s3.Bucket(this, config.getFullStackResourceName(this.name, 'output-bucket'))
 
         /** Create Glue tables */
@@ -41,11 +45,19 @@ export class EtlStack extends cdk.Stack {
             columns: eventColumns,
             dataFormat: glue.DataFormat.JSON,
         })
-        new glue.Table(this, config.getFullStackResourceName(this.name, 'agg-table'), {
+
+        new glue.Table(this, config.getFullStackResourceName(this.name, 'wallet-table'), {
             database: database,
-            tableName: snakeCase(config.getFullStackResourceName(this.name, 'agg-table')),
-            bucket: aggBucket,
-            columns: aggColumns,
+            tableName: snakeCase(config.getFullStackResourceName(this.name, 'wallet-table')),
+            bucket: walletBucket,
+            columns: walletColumns,
+            dataFormat: glue.DataFormat.JSON,
+        })
+        new glue.Table(this, config.getFullStackResourceName(this.name, 'staking-action-table'), {
+            database: database,
+            tableName: snakeCase(config.getFullStackResourceName(this.name, 'staking-action-table')),
+            bucket: stakingActionBucket,
+            columns: stakingActionColumns,
             dataFormat: glue.DataFormat.JSON,
         })
     }
