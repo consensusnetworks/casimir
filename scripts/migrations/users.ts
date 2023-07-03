03bdcbc707f9d13d5264bbbf8759d920b6403aa8
Shane Earley <shanejearley@gmail.com>
2023-07-03 14:04:53 -0400

    Add atlas to action dependencies

--- scripts/migrations/users.ts
+++ scripts/migrations/users.ts
@@ -1,9 +1,54 @@
-import { getSecret } from '@casimir/helpers'
+import fs from 'fs'
+import os from 'os'
+import { JsonSchema, Schema, accountSchema, nonceSchema, userAccountSchema, userSchema } from '@casimir/data'
+import { getSecret, run } from '@casimir/helpers'
 
 void async function () {
     const project = process.env.PROJECT || 'casimir'
     const stage = process.env.STAGE || 'dev'
     const service = 'users'
+
+    /** Load DB credentials */
     const dbCredentials = await getSecret(`${project}-${service}-db-credentials-${stage}`)
-    console.log(dbCredentials)
+    console.log('dbCredentials', dbCredentials)
+
+    /** Parse DB credentials */
+    const { dbname, port, host, username, password } = JSON.parse(dbCredentials as string)
+    const pgUrl = `postgres://${username}:${password}@${host}:${port}/${dbname}`
+
+    /** Resource path from package caller */
+    const resourcePath = './scripts'
+
+    /** All table schemas */
+    const tableSchemas = {
+        account: accountSchema,
+        nonce: nonceSchema,
+        user: userSchema,
+        userAccount: userAccountSchema
+    }
+    
+    /** Generate SQL schema */
+    let sqlSchema = ''
+    for (const table of Object.keys(tableSchemas)) {
+        const tableSchema = tableSchemas[table] as JsonSchema
+        const schema = new Schema(tableSchema)
+        const postgresTable = schema.getPostgresTable()
+        console.log(`${schema.getTitle()} JSON schema parsed to SQL`)
+        sqlSchema += `${postgresTable}\n\n`
+    }
+
+    /** Write schema to ${resourcePath}/.out/sql/schema.sql */
+    const sqlDir = `${resourcePath}/.out/sql`
+    if (!fs.existsSync(sqlDir)) fs.mkdirSync(sqlDir, { recursive: true })
+    fs.writeFileSync(`${sqlDir}/schema.sql`, sqlSchema)
+
+    const atlasCli = await run('which atlas')
+    if (!atlasCli) {
+        if (os.platform() === 'darwin') {
+            await run('echo y | brew install atlas')
+        } else {
+            await run('curl -sSf https://atlasgo.sh | sh')
+        }
+    }
+    await run(`atlas schema apply --url "${pgUrl}?sslmode=disable" --to "file://${sqlDir}/schema.sql" --dev-url "docker://postgres/15" --auto-approve`)
 }()
