e35898b469eb69ac2d792318f9dd314983f8ba79
Christopher Cali <chris@consensusnetworks.com>
2023-07-13 15:53:35 -0400

    Fix error thrown when calling getUserStake but user hasn't staked

--- apps/web/src/composables/contracts.ts
+++ apps/web/src/composables/contracts.ts
@@ -69,8 +69,8 @@ export default function useContracts() {
     async function getCurrentStaked(): Promise<BreakdownAmount> {
         const provider = new ethers.providers.JsonRpcProvider(ethereumUrl)
         const addresses = (user.value as UserWithAccounts).accounts.map((account: Account) => account.address) as string[]
-        const promises = addresses.map((address) => manager.connect(provider).getUserStake(address))
         try {
+            const promises = addresses.map((address) => manager.connect(provider).getUserStake(address))
             const settledPromises = await Promise.allSettled(promises) as Array<PromiseFulfilledResult<ethers.BigNumber>>
             const currentStaked = settledPromises
                 .filter((result) => result.status === 'fulfilled')
@@ -101,87 +101,88 @@ export default function useContracts() {
         return feesRounded
     }
 
-    async function getPools(address: string): Promise<Pool[]> {
-        const { user } = useUsers()
-        const provider = new ethers.providers.JsonRpcProvider(ethereumUrl)        
-        const userStake = await manager.connect(provider).getUserStake(address) // to get user's stake balance
-        const poolStake = await manager.connect(provider).getTotalStake() // to get total stake balance
-        const poolIds = [
-            ...await manager.connect(provider).getPendingPoolIds(),
-            ...await manager.connect(provider).getStakedPoolIds()
-        ]
+    // async function getPools(address: string): Promise<Pool[]> {
+    //     const { user } = useUsers()
+    //     const provider = new ethers.providers.JsonRpcProvider(ethereumUrl)        
+    //     const userStake = await manager.connect(provider).getUserStake(address) // to get user's stake balance
+    //     const poolStake = await manager.connect(provider).getTotalStake() // to get total stake balance
+    //     const poolIds = [
+    //         ...await manager.connect(provider).getPendingPoolIds(),
+    //         ...await manager.connect(provider).getStakedPoolIds()
+    //     ]
 
-        console.log('userStake :>> ', ethers.utils.formatEther(userStake))
-        console.log('poolStake :>> ', ethers.utils.formatEther(poolStake))
-        console.log('poolIds :>> ', poolIds)
+    //     console.log('userStake :>> ', ethers.utils.formatEther(userStake))
+    //     console.log('poolStake :>> ', ethers.utils.formatEther(poolStake))
+    //     console.log('poolIds :>> ', poolIds)
 
-        return await Promise.all(poolIds.map(async (poolId: number) => {
-            const { publicKey, operatorIds } = await views.connect(provider).getPoolDetails(poolId)
+    //     return await Promise.all(poolIds.map(async (poolId: number) => {
+    //         const { publicKey, operatorIds } = await views.connect(provider).getPoolDetails(poolId)
             
-            // TODO: Decide when/how to get rewards/userRewards
-            let pool: Pool = {
-                id: poolId,
-                rewards: ethers.utils.formatEther(poolStake),
-                stake: ethers.utils.formatEther(poolStake),
-                userRewards: ethers.utils.formatEther(userStake),
-                userStake: ethers.utils.formatEther(userStake)
-            }
+    //         // TODO: Decide when/how to get rewards/userRewards
+    //         let pool: Pool = {
+    //             id: poolId,
+    //             rewards: ethers.utils.formatEther(poolStake),
+    //             stake: ethers.utils.formatEther(poolStake),
+    //             userRewards: ethers.utils.formatEther(userStake),
+    //             userStake: ethers.utils.formatEther(userStake)
+    //         }
 
-            if (publicKey) {
-                // Validator data from beaconcha.in hardcoded for now
-                // const response = await fetch(`https://prater.beaconcha.in/api/v1/validator/${validatorPublicKey}`)
-                // const { data } = await response.json()
-                // const { status } = data
-                const validator = {
-                    publicKey,
-                    status: 'Active',
-                    effectiveness: '0%',
-                    apr: '0%', // See issue #205 https://github.com/consensusnetworks/casimir/issues/205#issuecomment-1338142532
-                    url: `https://prater.beaconcha.in/validator/${publicKey}`
-                }
+    //         if (publicKey) {
+    //             // Validator data from beaconcha.in hardcoded for now
+    //             // const response = await fetch(`https://prater.beaconcha.in/api/v1/validator/${validatorPublicKey}`)
+    //             // const { data } = await response.json()
+    //             // const { status } = data
+    //             const validator = {
+    //                 publicKey,
+    //                 status: 'Active',
+    //                 effectiveness: '0%',
+    //                 apr: '0%', // See issue #205 https://github.com/consensusnetworks/casimir/issues/205#issuecomment-1338142532
+    //                 url: `https://prater.beaconcha.in/validator/${publicKey}`
+    //             }
 
 
-                // TODO: Replace with less hardcoded network call?
-                const operators = await Promise.all(operatorIds.map(async (operatorId) => {
-                    const network = 'prater'
-                    const response = await fetch(`https://api.ssv.network/api/v3/${network}/operators/${operatorId}`)
-                    const { performance } = await response.json()
-                    return {
-                        id: operatorId.toNumber(),
-                        '24HourPerformance': performance['24h'],
-                        '30DayPerformance': performance['30d'],
-                        url: `https://explorer.ssv.network/operators/${operatorId}`
-                    }
-                }))
+    //             // TODO: Replace with less hardcoded network call?
+    //             const operators = await Promise.all(operatorIds.map(async (operatorId) => {
+    //                 const network = 'prater'
+    //                 const response = await fetch(`https://api.ssv.network/api/v3/${network}/operators/${operatorId}`)
+    //                 const { performance } = await response.json()
+    //                 return {
+    //                     id: operatorId.toNumber(),
+    //                     '24HourPerformance': performance['24h'],
+    //                     '30DayPerformance': performance['30d'],
+    //                     url: `https://explorer.ssv.network/operators/${operatorId}`
+    //                 }
+    //             }))
 
-                pool = {
-                    ...pool,
-                    validator,
-                    operators
-                }
-            }
+    //             pool = {
+    //                 ...pool,
+    //                 validator,
+    //                 operators
+    //             }
+    //         }
             
-            user.value?.accounts.forEach(account => {
-                if (account.address === address) {
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
-                    account.pools ? account.pools.push(pool) : account.pools = [pool]
-                }
-            })
+    //         user.value?.accounts.forEach(account => {
+    //             if (account.address === address) {
+    //                 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //                 // @ts-ignore
+    //                 account.pools ? account.pools.push(pool) : account.pools = [pool]
+    //             }
+    //         })
             
-            return pool
-        }))
-    }
+    //         return pool
+    //     }))
+    // }
 
     async function getAllTimeStakingRewards() : Promise<BreakdownAmount> {
         try {
             /* Get User's Current Stake */
             const addresses = (user.value as UserWithAccounts).accounts.map((account: Account) => account.address) as string[]
             const currentUserStakePromises = [] as Array<Promise<ethers.BigNumber>>
-            addresses.forEach((address) => {currentUserStakePromises.push(manager.connect(provider).getUserStake(address))})
-            const resolvedCurrentUserStakePromises = await Promise.all(currentUserStakePromises)
-            const currentUserStake = resolvedCurrentUserStakePromises.reduce((accumulator, currentValue) => accumulator.add(currentValue), ethers.BigNumber.from(0))
-            const currentUserStakeETH = parseFloat(ethers.utils.formatEther(currentUserStake))
+            addresses.forEach(address => currentUserStakePromises.push(manager.connect(provider).getUserStake(address)))
+            const settledCurrentUserStakePromises = await Promise.allSettled(currentUserStakePromises) as Array<PromiseFulfilledResult<ethers.BigNumber>>
+            const currentUserStake = settledCurrentUserStakePromises.filter(result => result.status === 'fulfilled').map(result => result.value)
+            const currentUserStakeSum = currentUserStake.reduce((acc, curr) => acc.add(curr), ethers.BigNumber.from(0))
+            const currentUserStakeETH = parseFloat(ethers.utils.formatEther(currentUserStakeSum))
 
             /* Get User's All Time Deposits and Withdrawals */
             const userEventTotalsPromises = [] as Array<Promise<ContractEventsByAddress>>
@@ -204,7 +205,7 @@ export default function useContracts() {
                 usd: formatNumber(currentUserStakeMinusEvents * (await getCurrentPrice({ coin: 'ETH', currency: 'USD' })))
             }
         } catch (err) {
-            console.error('err :>> ', err)
+            console.error(`There was an error in getAllTimeStakingRewards: ${err}`)
             return {
                 exchange: '0 ETH',
                 usd: '$ 0.00'
@@ -227,45 +228,57 @@ export default function useContracts() {
     }
 
     async function getContractEventsTotalsByAddress(address: string) : Promise<ContractEventsByAddress> {
-        const provider = new ethers.providers.JsonRpcProvider(ethereumUrl)
-        const eventList = [
-            'StakeDeposited',
-            'StakeRebalanced',
-            'WithdrawalInitiated'
-        ]
-        const eventFilters = eventList.map(event => {
-            if (event === 'StakeRebalanced') return manager.filters[event]()
-            return manager.filters[event](address)
-        })
-        const items = (await Promise.all(eventFilters.map(async eventFilter => await manager.connect(provider).queryFilter(eventFilter, 0, 'latest'))))
-
-        const userEventTotals = eventList.reduce((acc, event) => {
-            acc[event] = 0
-            return acc
-        }, {} as { [key: string]: number })
-
-        for (const item of items) {
-            for (const action of item) {
-                const { args, event } = action
-                const { amount } = args
-                const amountInEth = parseFloat(ethers.utils.formatEther(amount))
-                userEventTotals[event as string] += amountInEth
+        try {
+            const provider = new ethers.providers.JsonRpcProvider(ethereumUrl)
+            const eventList = [
+                'StakeDeposited',
+                'StakeRebalanced',
+                'WithdrawalInitiated'
+            ]
+            const eventFilters = eventList.map(event => {
+                if (event === 'StakeRebalanced') return manager.filters[event]()
+                return manager.filters[event](address)
+            })
+            const items = (await Promise.all(eventFilters.map(async eventFilter => await manager.connect(provider).queryFilter(eventFilter, 0, 'latest'))))
+    
+            const userEventTotals = eventList.reduce((acc, event) => {
+                acc[event] = 0
+                return acc
+            }, {} as { [key: string]: number })
+    
+            for (const item of items) {
+                for (const action of item) {
+                    const { args, event } = action
+                    const { amount } = args
+                    const amountInEth = parseFloat(ethers.utils.formatEther(amount))
+                    userEventTotals[event as string] += amountInEth
+                }
             }
+    
+            return userEventTotals
+        } catch (err) {
+            console.error(`There was an error in getContractEventsTotalsByAddress: ${err}`)
         }
-
-        return userEventTotals
     }
 
     async function listenForContractEvents() {
-        manager.on('StakeDeposited', async () => await refreshBreakdown())
-        manager.on('StakeRebalanced', async () => await refreshBreakdown())
-        manager.on('WithdrawalInitiated', async () => await refreshBreakdown())
+        try {
+            manager.on('StakeDeposited', async () => await refreshBreakdown())
+            manager.on('StakeRebalanced', async () => await refreshBreakdown())
+            manager.on('WithdrawalInitiated', async () => await refreshBreakdown())
+        } catch (err) {
+            console.log(`There was an error in listenForContractEvents: ${err}`)
+        }
     }
 
     async function refreshBreakdown() {
-        setBreakdownValue({ name: 'currentStaked', ...await getCurrentStaked() })
-        setBreakdownValue({ name: 'totalWalletBalance', ...await getTotalWalletBalance() })
-        setBreakdownValue({ name: 'stakingRewardsEarned', ...await getAllTimeStakingRewards() })
+        try {
+            setBreakdownValue({ name: 'currentStaked', ...await getCurrentStaked() })
+            setBreakdownValue({ name: 'totalWalletBalance', ...await getTotalWalletBalance() })
+            setBreakdownValue({ name: 'stakingRewardsEarned', ...await getAllTimeStakingRewards() })
+        } catch (err) {
+            console.log(`There was an error in refreshBreakdown: ${err}`)
+        }
     }
 
     function setBreakdownValue({ name, exchange, usd }: { name: BreakdownString, exchange: string, usd: string}) {
@@ -317,7 +330,7 @@ export default function useContracts() {
         deposit, 
         getCurrentStaked,
         getDepositFees, 
-        getPools, 
+        // getPools, 
         listenForContractEvents,
         refreshBreakdown,
         withdraw 
