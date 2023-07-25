11c9a84ada131638869d2948ef94cdc3f550d23f
demogorgod <ssnajeeb2020@manchester.edu>
2023-07-24 22:21:10 -0400

    Add mobile layout to the app

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,22 +1,40 @@
 <script lang="ts" setup>
   import { RouterView } from 'vue-router'
   import DefaultLayout from '@/layouts/default-layout.vue'
+  import MobileLayout from '@/layouts/mobile-layout.vue'
   import useTxData from '@/mockData/mock_transaction_data'
-  import { onMounted } from 'vue'
+  import { onMounted, onUnmounted, ref} from 'vue'
   import useUsers from '@/composables/users'
 
   const { mockData } = useTxData()
   // Need this to fix initilizing bug between user.ts and ssv.ts
   const { user } = useUsers()
+
+  const screenWidth = ref(0)
+
+  // Made this to be 600 due to how our components look, I think anything below 600 would look best in a mobile view (tablets and below)
+  const mobileWidthLimits = ref(600)
+
+  const findScreenDimenstions = () => {
+    screenWidth.value = window.innerWidth
+  }
+
   onMounted(() => {
+    findScreenDimenstions()
+
+    window.addEventListener('resize', findScreenDimenstions)
     mockData()
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', findScreenDimenstions)
   })
 </script>
   
 <template>
   <Suspense>
     <component
-      :is="DefaultLayout"
+      :is="screenWidth <= mobileWidthLimits? MobileLayout : DefaultLayout"
     >
       <RouterView />
     </component>
