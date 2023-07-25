714bc056822cd412b135c5c27f15df15bc6b4782
demogorgod <ssnajeeb2020@manchester.edu>
2023-07-24 23:34:06 -0400

    Add nav items to the nav bar

--- apps/web/src/layouts/mobile-layout.vue
+++ apps/web/src/layouts/mobile-layout.vue
@@ -1,16 +1,74 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import VueFeather from 'vue-feather'
+
+// import BreakdownChart from '@/pages/overview/components/BreakdownChart.vue'
+
+</script>
 
 <template>
-  <div class="h-[100vh] w-[100vw]">
+  <div class="h-[100vh] w-[100vw] min-w-[360px]">
     <div class="flex flex-col h-full w-full">
-      <div class="h-full w-full">
-        page
+      <div class="h-full w-full ">
+        a;lskdjfak;l
       </div>
-      <div class="h-[70px] w-full border border-decline">
-        nav
+      <div class="h-[70px] w-full bg-black text-white flex justify-around items-center">
+        <div class="nav_item">
+          <vue-feather
+            type="settings"
+            class="icon w-[16px] h-min"
+          />
+          <span>Settings</span>
+        </div>
+
+        <div class="nav_item">
+          <vue-feather
+            type="layout"
+            class="icon w-[16px] h-min"
+          />
+          <span>Breakdown</span>
+        </div>
+
+        <div class="nav_item">
+          <vue-feather
+            type="download"
+            class="icon w-[16px] h-min"
+          />
+          <span>Stake</span>
+        </div>
+
+        <div class="nav_item">
+          <vue-feather
+            type="table"
+            class="icon w-[16px] h-min"
+          />
+          <span>Account</span>
+        </div>
+
+        <div class="nav_item">
+          <vue-feather
+            type="credit-card"
+            class="icon w-[16px] h-min"
+          />
+          <span>Wallets</span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.nav_item{
+    color: #FFF;
+    font-family: IBM Plex Sans;
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: 0.13px;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    justify-content: center;
+    align-items: center;
+}
+</style>
