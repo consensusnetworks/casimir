b221f11c7abffc5986df1de8b4394cefafb6f868
demogorgod <ssnajeeb2020@manchester.edu>
2023-07-25 22:19:20 -0400

    Add slide animation to each tab

--- apps/web/src/layouts/mobile-layout.vue
+++ apps/web/src/layouts/mobile-layout.vue
@@ -1,73 +1,135 @@
 <script lang="ts" setup>
+import { ref, watch } from 'vue'
 import VueFeather from 'vue-feather'
 
 // import BreakdownChart from '@/pages/overview/components/BreakdownChart.vue'
 
+// @Chris can we save this variable in storage for the user so on refresh they hop on the same view as the last
+const activeView = ref('wallets')
+// const slideAnimtaion = ref(false)
+
+// watch(activeView, () => {
+//     slideAnimtaion.value = true 
+//     setTimeout(() => {
+//         slideAnimtaion.value = false
+//     }, 500)
+// })
 </script>
 
 <template>
   <div class="h-[100vh] w-[100vw] min-w-[360px]">
-    <div class="flex flex-col h-full w-full">
-      <div class="h-full w-full ">
-        a;lskdjfak;l
+    <div class="flex flex-col h-full w-full relative">
+      <div class="h-full w-full flex items-center justify-center absolute top-0 left-0 z-[1] bg-black">
+        <img
+          src="/casimir.svg"
+          alt="Casimir Background Logo"
+          class=""
+        >
       </div>
-      <div class="h-[70px] w-full bg-black text-white flex justify-around items-center">
-        <div class="nav_item">
-          <vue-feather
-            type="settings"
-            class="icon w-[16px] h-min"
-          />
-          <span>Settings</span>
-        </div>
+      <div class="h-full w-full z-[2] overflow-hidden">
+        <transition name="slide-up">
+          <div
+            v-if="activeView === 'wallets'"
+            key="wallets"
+            class="w-full h-full bg-blue-400"
+          >
+            {{ activeView }}
+          </div>
+          <div
+            v-else-if="activeView === 'breakdown'"
+            key="breakdown"
+            class="w-full h-full bg-red-400"
+          >
+            {{ activeView }}
+          </div>
+        </transition>
+      </div>
 
-        <div class="nav_item">
-          <vue-feather
-            type="layout"
-            class="icon w-[16px] h-min"
-          />
-          <span>Breakdown</span>
-        </div>
-
-        <div class="nav_item">
-          <vue-feather
-            type="download"
-            class="icon w-[16px] h-min"
-          />
-          <span>Stake</span>
-        </div>
-
-        <div class="nav_item">
-          <vue-feather
-            type="table"
-            class="icon w-[16px] h-min"
-          />
-          <span>Account</span>
-        </div>
-
-        <div class="nav_item">
+      <!-- Nav Bar -->
+      <div class="h-[70px] w-full bg-black text-white flex justify-around items-center z-[3]">
+        <button
+          class="nav_item"
+          :class="activeView === 'wallets'? 'text-white' : 'text-[#898a8c]'"
+          @click="activeView = 'wallets'"
+        >
           <vue-feather
             type="credit-card"
             class="icon w-[16px] h-min"
           />
           <span>Wallets</span>
-        </div>
+        </button>
+
+        <button
+          class="nav_item"
+          :class="activeView === 'breakdown'? 'text-white' : 'text-[#898a8c]'"
+          @click="activeView = 'breakdown'"
+        >
+          <vue-feather
+            type="layout"
+            class="icon w-[16px] h-min"
+          />
+          <span>Breakdown</span>
+        </button>
+
+        <button
+          class="nav_item"
+          :class="activeView === 'stake'? 'text-white' : 'text-[#898a8c]'"
+          @click="activeView = 'stake'"
+        >
+          <vue-feather
+            type="download"
+            class="icon w-[16px] h-min"
+          />
+          <span>Stake</span>
+        </button>
+
+        <button
+          class="nav_item"
+          :class="activeView === 'accounts'? 'text-white' : 'text-[#898a8c]'"
+          @click="activeView = 'accounts'"
+        >
+          <vue-feather
+            type="table"
+            class="icon w-[16px] h-min"
+          />
+          <span>Accounts</span>
+        </button>
+
+        <button
+          class="nav_item"
+          :class="activeView === 'settings'? 'text-white' : 'text-[#898a8c]'"
+          @click="activeView = 'settings'"
+        >
+          <vue-feather
+            type="settings"
+            class="icon w-[16px] h-min"
+          />
+          <span>Settings</span>
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.slide-up-enter-to{
+    transform: translateY(0%);
+    transition: 0.3s ease-in;
+}
+
+.slide-up-enter-from{
+    transform: translateY(100%);
+}
+
 .nav_item{
-    color: #FFF;
-    font-family: IBM Plex Sans;
     font-size: 10px;
     font-style: normal;
-    font-weight: 400;
+    font-weight: 600;
     line-height: normal;
-    letter-spacing: 0.13px;
+    letter-spacing: 1px;
     display: flex;
     flex-direction: column;
-    gap: 5px;
+    gap: 8px;
     justify-content: center;
     align-items: center;
 }
