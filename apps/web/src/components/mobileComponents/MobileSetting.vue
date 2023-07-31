a59e1d32ca68577ddb7fe9357ea8c694862323fa
demogorgod <ssnajeeb2020@manchester.edu>
2023-07-31 15:17:46 -0400

    Add setting tab in mobile view

--- apps/web/src/components/mobileComponents/MobileSetting.vue
+++ apps/web/src/components/mobileComponents/MobileSetting.vue
@@ -1,11 +1,75 @@
 <script lang="ts" setup>
+import VueFeather from 'vue-feather'
+import useWallet from '@/composables/wallet'
+import useUser from '@/composables/users'
+
+
+const  {
+  logout,
+} = useWallet()
+
+const { user } = useUser()
 </script>
 
 <template>
   <div
-    class="h-full w-full"
+    class="h-full w-full px-[20px] pb-[70px] pt-[15px]"
   >
-    settings
+    <button class="border-b border-[#EAECF0] flex items-center px-[16px] py-[10px] gap-[12px] w-full">
+      <vue-feather
+        type="user"
+        size="36"
+        class="icon w-[17px] h-min"
+      />
+            
+      <span>
+        Account
+      </span>
+    </button>
+    <button class="flex items-center px-[16px] py-[10px] gap-[12px] w-full">
+      <vue-feather
+        type="layers"
+        size="36"
+        class="icon w-[17px] h-min"
+      />
+      <span>
+        Chanelog
+      </span>
+    </button>
+    <button class="flex items-center px-[16px] py-[10px] gap-[12px] w-full">
+      <vue-feather
+        type="help-circle"
+        size="36"
+        class="icon w-[17px] h-min"
+      />
+      <span>
+        Support
+      </span>
+    </button>
+    <button class="flex items-center px-[16px] py-[10px] gap-[12px] w-full">
+      <vue-feather
+        type="code"
+        size="36"
+        class="icon w-[17px] h-min"
+      />
+      <span>
+        API
+      </span>
+    </button>
+    <button
+      class="border-t border-[#EAECF0] flex items-center px-[16px] py-[10px] gap-[12px] w-full"
+      :disabled="!user"
+      @click="logout"
+    >
+      <vue-feather
+        type="log-out"
+        size="36"
+        class="icon w-[17px] h-min"
+      />
+      <span>
+        Log out
+      </span>
+    </button>
   </div>
 </template>
 
