26a0ba74ae4631f9cc079c920289348d829ec8e3
Shane Earley <shanejearley@gmail.com>
2023-08-03 22:50:02 -0400

    Update environment file in web

--- apps/web/src/composables/environment.ts
+++ apps/web/src/composables/environment.ts
@@ -1,8 +1,10 @@
 export default function useEnvironment() {
     const domain = window.location.host
     const origin = window.location.origin
-    const managerAddress = import.meta.env.PUBLIC_MANAGER_ADDRESS || '0x07e05700cb4e946ba50244e27f01805354cd8ef0'
-    const viewsAddress = import.meta.env.PUBLIC_VIEWS_ADDRESS || '0x6b34d231b467fccebdc766187f7251795281dc26'
+    const managerAddress = import.meta.env.PUBLIC_MANAGER_ADDRESS
+    if (!managerAddress) throw new Error('No manager address provided')
+    const viewsAddress = import.meta.env.PUBLIC_VIEWS_ADDRESS
+    if (!viewsAddress) throw new Error('No views address provided')
     const usersUrl = import.meta.env.PUBLIC_USERS_URL || 'http://localhost:4000'
     const ethereumUrl = import.meta.env.PUBLIC_ETHEREUM_RPC_URL || 'http://127.0.0.1:8545'
     const ledgerType = import.meta.env.PUBLIC_SPECULOS_URL ? 'speculos' : 'usb'
