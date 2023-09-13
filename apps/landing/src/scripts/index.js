364d285f1ce235b703b8218a7a72573da0a9bf90
hawyar <hawyarfa@gmail.com>
2023-09-13 17:21:32 -0400

    Style hero cards

--- apps/landing/src/scripts/index.js
+++ apps/landing/src/scripts/index.js
@@ -1,49 +1,59 @@
-import { animate } from "motion";
-
 const slots = document.querySelector(".slots");
 const newsletterInput = document.querySelector("#newsletter-input");
 const newsletterSubmit = document.querySelector("#newsletter-submit");
 
-newsletterSubmit.addEventListener("click", (event) => {
-  event.preventDefault();
-  const email = newsletterInput.value;
-  console.log(email);
-  newsletterInput.value = "";
+
+if (newsletterInput && newsletterSubmit) {
+  newsletterSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    const email = newsletterInput.value;
+    console.log(email);
+    newsletterInput.value = "";
+  });
+}
+
+const eventTypes = ["transaction", "transfer", "deposit", "withdrawal", "Restaking", "Staking"];
+
+const events = Array.from({ length: 12 }, () => {
+  return {
+    type: eventTypes[Math.floor(Math.random() * eventTypes.length)],
+    amount: Math.floor(Math.random() * 1000),
+    date: "2021-01-01",
+    from: "0x" + Math.floor(Math.random() * 16777215).toString(16),
+    to: "0x" + Math.floor(Math.random() * 16777215).toString(16),
+  };
 });
 
+
 function newSlot() {
+  const randomEvent = Math.floor(Math.random() * events.length);
+  const event = events[randomEvent];
+
+
   const slot = document.createElement("div");
-  const span = document.createElement("span");
-
-  span.textContent = "New Transaction";
-
+  const pill = document.createElement("span");
+  const slotObj = document.createElement("div");
+  const key = document.createElement("span");
+  const value = document.createElement("span");
 
   slot.classList.add("slot");
-  span.classList.add("highlight");
-  span.classList.add("text-10");
+  pill.classList.add("pill");
+  slotObj.classList.add("slot__obj");
+  key.classList.add("text-9");
+  value.classList.add("text-9");
 
-  slot.appendChild(span);
+
+  pill.innerText = event.type;
+  key.innerText = "From";
+  value.innerText = event.from;
+
+  slotObj.appendChild(key);
+  slotObj.appendChild(value);
+  slot.appendChild(pill);
+  slot.appendChild(slotObj);
   slots.appendChild(slot);
 }
 
 [...Array(12)].forEach((_, i) => {
   newSlot();
-});
-
-slots.childNodes.forEach(slot => {
-  slot.addEventListener("mouseenter", (event) => {
-    animate(slot, {
-      x: -50,
-      y: -45,
-      rotate: -2,
-    });
-  });
-
-  slot.addEventListener("mouseleave", (event) => {
-    animate(slot, {
-      x: 0,
-      y: 0,
-      rotate: 0,
-    });
-  });
 });
