f9a56eb53818e19adf904d77d29b650f40df9460
demogorgod <ssnajeeb2020@manchester.edu>
2023-09-29 18:20:31 -0400

    Add blog page to pages and router

--- apps/landing/src/composables/router.ts
+++ apps/landing/src/composables/router.ts
@@ -1,10 +1,17 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Landing from '@/pages/landing/Landing.vue'
+import Blog from '@/pages/blog/Blog.vue'
 const routes = [
   {
     path: '/',
     name: Landing,
     component: Landing,
+  },
+  {
+    path: '/blog',
+    name: Blog,
+    component: Blog,
+    // children: []
   },
 ]
 
---
+++ apps/landing/src/pages/blog/Blog.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <nav class="nav">
+      <div class="nav__container">
+        <a href="/">
+          <img
+            class="logo"
+            src="/logo.svg"
+          >
+        </a>
+        <ul class="nav__links">
+          <li>
+            <a href="https://github.com/consensusnetworks/casimir#casimir">API Reference</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/changelog">Changelog</a>
+          </li>
+          <li>
+            <a href="https://consensusnetworks.com">Company</a>
+          </li>
+        </ul>
+        <a
+          href="https://app.dev.casimir.co"
+          class="btn-primary-sm"
+        >
+          Launch App
+        </a>
+      </div>
+    </nav>
+
+    <section class="blog">
+      <div class="blog__container">
+        <h1 class="text-2">
+          Blog
+        </h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<style scoped></style>
