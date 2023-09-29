52339dfc4fbccd134e559b39bd8183b85a980c43
demogorgod <ssnajeeb2020@manchester.edu>
2023-09-28 21:39:31 -0400

    migrate index.html to app.vue and index.scss into index.css

--- apps/landing/src/composables/router.ts
+++ apps/landing/src/composables/router.ts
@@ -1,19 +1,18 @@
-import Landing from '@/pages/landing/Landing.vue'
 import { createWebHistory, createRouter } from 'vue-router'
 
-const routes = [ 
-    { 
-        path: '/', 
-        name: Landing,
-        component: Landing,
-    },
+const routes = [
+  // {
+  //     path: '/',
+  //     name: ,
+  //     component: ,
+  // },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    routes
+  history: createWebHistory(),
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  routes,
 })
 
 export default router
