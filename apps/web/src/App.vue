f999f2a4e5130f47c96d83cae737a63e93217c80
demogorgod <ssnajeeb2020@manchester.edu>
2023-10-10 17:01:42 -0400

    Change indent spaces from 4 to 2 && changed max-len to 999 due to errors

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,13 +1,15 @@
 <script lang="ts" setup>
-  import { RouterView } from 'vue-router'
-  import DefaultLayout from '@/layouts/default-layout.vue'
+import { RouterView } from 'vue-router'
+import DefaultLayout from '@/layouts/default-layout.vue'
+
+function helloChris() {
+  console.log('sjfd;askdnmj')
+}
 </script>
   
 <template>
   <Suspense>
-    <component
-      :is="DefaultLayout"
-    >
+    <component :is="DefaultLayout">
       <RouterView />
     </component>
   </Suspense>
