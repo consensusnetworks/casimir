cb3b9d12ce51be1c2fdbf8679cbb43dd6c302c12
Christopher Cali <chris@consensusnetworks.com>
2023-10-19 14:54:22 -0400

    Implement contract factory approach in staking and breakdownMetrics composables/component

--- apps/web/src/composables/staking.ts
+++ apps/web/src/composables/staking.ts
@@ -5,12 +5,19 @@ import useEthers from '@/composables/ethers'
 import useLedger from '@/composables/ledger'
 import useTrezor from '@/composables/trezor'
 import useWalletConnectV2 from './walletConnectV2'
+import ICasimirManagerAbi from '@casimir/ethereum/build/abi/ICasimirManager.json'
+import { CasimirManager } from '@casimir/ethereum/build/@types'
 
-const { manager } = useEnvironment()
+const { factory, provider } = useEnvironment()
 const { ethersProviderList, getEthersBrowserSigner } = useEthers()
 const { getEthersLedgerSigner } = useLedger()
 const { getEthersTrezorSigner } = useTrezor()
 const { getWalletConnectSignerV2 } = useWalletConnectV2()
+
+const managerConfigs = await Promise.all((await factory.getManagerIds()).map(async (id: number) => {
+    return await factory.getManagerConfig(id)
+}))
+const manager = new ethers.Contract(managerConfigs[0].managerAddress, ICasimirManagerAbi, provider) as CasimirManager
 
 export default function useStaking() {
     
