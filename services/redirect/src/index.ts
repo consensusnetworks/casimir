7b5261752a71ddc9a3443890bb09bb7ff5db1877
Shane Earley <shanejearley@gmail.com>
2023-10-30 18:21:36 -0400

    Update cloudfront function source

--- services/redirect/src/index.ts
+++ services/redirect/src/index.ts
@@ -1,22 +1,14 @@
-import path from 'path'
-import { CloudFrontRequestHandler } from 'aws-lambda'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function handler(event: AWSCloudFrontFunction.Event) {
+    const request = event.request
+    const uri = request.uri
 
-export const handler: CloudFrontRequestHandler = (event, context, callback) => {
-    const { request } = event.Records[0].cf
-    console.log('Request URI', request.uri)
-
-    const parsedPath = path.parse(request.uri)
-    console.log('Parsed Path', parsedPath)
-    
-    let newUri: string
-    if (parsedPath.ext === '') {
-        newUri = path.join(parsedPath.dir, parsedPath.base, '.html')
-    } else {
-        newUri = request.uri
+    if (uri.endsWith('/')) {
+        request.uri += 'index.html'
+    }
+    else if (!uri.includes('.')) {
+        request.uri += '/index.html'
     }
 
-    console.log('New URI ', newUri)
-    request.uri = newUri
-
-    return callback(null, request)
+    return request
 }
