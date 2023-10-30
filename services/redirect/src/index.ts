df0045b19783e37c07fa05df4831b2ea495cf352
Shane Earley <shanejearley@gmail.com>
2023-10-30 18:30:39 -0400

    Change functions rewrites to s3 structure

--- services/redirect/src/index.ts
+++ services/redirect/src/index.ts
@@ -1,14 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function handler(event: AWSCloudFrontFunction.Event) {
     const request = event.request
+
     const uri = request.uri
-
-    if (uri.endsWith('/')) {
-        request.uri += 'index.html'
+    if (!uri.includes('.')) {
+        request.uri += '.html'
     }
-    else if (!uri.includes('.')) {
-        request.uri += '/index.html'
-    }
-
+    
     return request
 }
