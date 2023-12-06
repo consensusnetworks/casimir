5bddb9b277ee22d4b30328d9fc5da92cedbb9ceb
Shane Earley <shanejearley@gmail.com>
2023-12-06 13:53:33 -0500

    Unpause contract

--- contracts/ethereum/scripts/report.ts
+++ contracts/ethereum/scripts/report.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat"
-import { CasimirFactory, CasimirManager, CasimirUpkeep } from "../build/@types"
+import { CasimirFactory, CasimirUpkeep } from "../build/@types"
 import { waitForNetwork } from "../helpers/network"
 
 void async function() {
@@ -11,12 +11,8 @@ void async function() {
     const factory = await ethers.getContractAt("CasimirFactory", process.env.FACTORY_ADDRESS) as CasimirFactory
     const [managerId] = await factory.getManagerIds()
     const managerConfig = await factory.getManagerConfig(managerId)
-    const manager = await ethers.getContractAt("CasimirManager", managerConfig.managerAddress) as CasimirManager
     const upkeep = await ethers.getContractAt("CasimirUpkeep", managerConfig.upkeepAddress) as CasimirUpkeep
 
     const requestReport = await upkeep.connect(owner).requestReport()
     await requestReport.wait()
-
-    // const unpause = await manager.connect(owner).setPaused(true)
-    // await unpause.wait()
 }()
