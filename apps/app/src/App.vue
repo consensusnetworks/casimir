f7b57e08715b12fb48dd33e549010277ef16f480
demogorgod <ssnajeeb2020@manchester.edu>
2023-12-11 16:22:54 -0500

    Add vueUse and implement dark/light mode functionality

--- apps/app/src/App.vue
+++ apps/app/src/App.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
+<script setup>
+import { useDark, useToggle } from "@vueuse/core"
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <div>
-    Basic App
+  <div class="text-white dark:text-red-500">
+    Basic App {{ isDark }}
+    <button @click="toggleDark()">
+      toggle light/dark mode
+    </button>
   </div>
 </template>
 
