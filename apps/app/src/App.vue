a6ee278751ddb107684fe2e085caaa528cd26131
demogorgod <ssnajeeb2020@manchester.edu>
2023-12-11 22:15:51 -0500

    Add inter as a font family and set up base elements styling

--- apps/app/src/App.vue
+++ apps/app/src/App.vue
@@ -20,6 +20,15 @@ const acceptTermsStorage = useStorage("acceptTerms", acceptTerms)
     <button @click="acceptTerms = !acceptTerms">
       toggle terms of service {{ acceptTermsStorage }}
     </button>
+
+    <h1>Header 1</h1>
+    <h2>Header 2</h2>
+    <h3>Header 3</h3>
+    <h4>Header 4</h4>
+    <h5>Header 5</h5>
+    <h6>Header 6</h6>
+    <p>Paragraph</p>
+    <small>Small Text</small>
   </div>
 </template>
 
