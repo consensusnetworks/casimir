a3b79dcafd3247461b5a69503dba3cbbe22c49e8
demogorgod <ssnajeeb2020@manchester.edu>
2023-12-14 11:45:56 -0500

    Add possible routes to router.ts

--- apps/app/src/composables/services/router.ts
+++ apps/app/src/composables/services/router.ts
@@ -5,7 +5,44 @@ const routes = [
         path: "/", 
         // name: Overview,
         // component: Overview,
+        // children: [
+        // { 
+        //     path: "/welcome", 
+        //     name: Overview,
+        //     component: Overview,
+        // },
+        // { 
+        //     path: "/welcome-back", 
+        //     name: Overview,
+        //     component: Overview,
+        // },
+        // ]
     },
+    // { 
+    // path: "/overview", 
+    // name: Overview,
+    // component: Overview,
+    // },
+    // { 
+    //     path: "/stake", 
+    // name: Stake,
+    // component: Stake,
+    // },
+    // { 
+    //     path: "/explore", 
+    // name: Explore,
+    // component: Explore,
+    // },
+    // { 
+    //     path: "/settings"
+    // name: Settings,
+    // component: Settings,
+    // },
+    // { 
+    //     path: "/page-not-found", 
+    // name: PageNotFound,
+    // component: PageNotFound,
+    // },
 ]
 
 
